e83ab843fd7ad032b12619bae85e9170414de41d
Rohit Raj <devrohitraj@users.noreply.github.com>
2016-03-16 14:27:30 +0530

    Update FlightController.java

--- src/com/raj/flight/controller/FlightController.java
+++ src/com/raj/flight/controller/FlightController.java
@@ -1,4 +1,4 @@
-package com.raj.flight.controller;
+package com.rohit.flight.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
